0fb352706114cd597fd77c2bd6094c2a002edf0a
Samantha Goodman <slgoodman89@gmail.com>
2020-10-09 21:34:50 -0400

    added form to Login with handleChange and started submit

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,54 @@
-import React from "react";
+import React, {useState} from "react";
+import { useHistory } from "react-router-dom";
 
-const Login = () => {
+const inititialData = {
+  username: '',
+  password: ''
+}
+
+const Login = (props) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [login, setLogin] =  useState(inititialData)
+  const {push} = useHistory();
+
+  const handleChange = e => {
+    setLogin({
+      ...login,
+      [e.target.name]: e.target.value
+    })
+    console.log(login)
+  }
+
+  const handleSubmit  = e => {
+    e.preventDefault();
+    /* uselogin function that add check login, adds token if good and sends user to bubblepage route */
+  }
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
       <p>Build a login page here</p>
+      <form onSubmit={handleSubmit}>
+        <lable>Username:
+            <input 
+              type='text' 
+              name='username' 
+              value={login.username}
+              onChange={handleChange}
+
+            />
+        </lable>
+        <lable>Password:
+            <input 
+              type='password' 
+              name='password' 
+              value={login.password}
+              onChange={handleChange}
+            />
+        </lable>
+        <button>Login</button>
+      </form>
     </>
   );
 };
