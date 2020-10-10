7a5932c2d1634db91382118fbdbc17248789943e
Samantha Goodman <slgoodman89@gmail.com>
2020-10-09 22:20:42 -0400

    login is working with private route woohoo

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,25 @@
 import React, { useEffect, useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import axios from 'axios';
 
 import Login from "./components/Login";
 import "./styles.scss";
+import BubblePage from "./components/BubblePage";
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
 
   return (
+    
     <Router>
       <div className="App">
+        <Link to='/bubble-page'>To the Bubbles!</Link>
         <Route exact path="/" component={Login} />
         {/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
+        <PrivateRoute path='/bubble-page' component={BubblePage} />
       </div>
     </Router>
   );
